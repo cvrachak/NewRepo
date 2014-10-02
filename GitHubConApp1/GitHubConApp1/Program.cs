8301c679c57a2ab52b0d311e2565ef30795c619c
cvrachak <cvrachak@gmail.com>
2014-10-02 14:25:28 -0500

    go for it now.

---
+++ GitHubConApp1/GitHubConApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubConApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World ::" + DateTime.Now);
+        }
+    }
+}
