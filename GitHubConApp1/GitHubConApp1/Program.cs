93cdb685a191bb6ce22a94ae5faa33879de5d130
cvrachak <cvrachak@gmail.com>
2015-12-24 23:35:08 -0600

    removed the changes made
    here you go

--- GitHubConApp1/GitHubConApp1/Program.cs
+++ GitHubConApp1/GitHubConApp1/Program.cs
@@ -6,9 +6,7 @@ namespace GitHubConApp1
     {
         static void Main()
         {
-            Console.WriteLine("Started");
             Console.WriteLine("Git Hub added and now edited after that Hello World ::" + DateTime.Now);
-            Console.WriteLine("Completed not yet");
         }
     }
 }
